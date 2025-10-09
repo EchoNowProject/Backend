0617d8b7767a6acc611960acde1728cdfe5abcf4
Antonio Blazquez <antblajim@gmail.com>
2025-10-09 22:26:49 +0200

    FEAT - SE AGERGAN LOS MODELOS

---
+++ app/Models/Base/User.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models\Base;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+
+
+/**
+ * Class User
+ * 
+ * @property int $id
+ * @property string $username
+ * @property string $email
+ * @property string $password
+ * @property string|null $display_name
+ * @property string|null $biography
+ * @property string|null $avatar_img
+ * @property bool $verified
+ * @property int $status
+ * @property int|null $plan
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ *
+ * @package App\Models\Base
+ */
+
+// ? Puede que el Authenticatable falle
+class User extends Model implements Authenticatable
+{
+	use HasFactory;
+
+	protected $table = 'users';
+
+	protected $casts = [
+		'verified' => 'bool',
+		'status' => 'int',
+		'plan' => 'int'
+	];
+}
