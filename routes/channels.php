d1b6d70394703712b8f44ed4c0bec454496de5bc
Antonio Blazquez <antblajim@gmail.com>
2026-05-18 18:04:12 +0200

    #40 - feat: Websocket integrated

--- routes/channels.php
+++ routes/channels.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Models\GroupChatConversation;
+use App\Models\Server;
+use App\Models\ServerChatConversation;
+use App\Models\ServerMember;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
@@ -29,5 +32,28 @@ Broadcast::channel('group-chat.{idConversation}', function ($user, $idConversati
 
     return $conversation->participants()
         ->where('users.id', $user->id)
+        ->exists();
+});
+
+Broadcast::channel('server-chat.{idServer}.{idConversation}', function ($user, $idServer, $idConversation) {
+
+    $conversation = ServerChatConversation::where('id_server', $idServer)->where('id', $idConversation)->first();
+
+    if (!$conversation) {
+        return false;
+    }
+
+    $server = Server::find($idServer);
+
+    if (!$server) {
+        return false;
+    }
+
+    if ($server->owner_id === $user->id) {
+        return true;
+    }
+
+    return ServerMember::where('server_id', $idServer)
+        ->where('user_id', $user->id)
         ->exists();
 });
